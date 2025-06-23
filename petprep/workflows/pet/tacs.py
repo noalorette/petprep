dd277ff1ecfd39861513572ce8c126c17b45b20d
mnoergaard <martin.noergaard@nru.dk>
2025-06-23 14:52:52 +0200

    FIX: Update tacs workflow and inputs

--- petprep/workflows/pet/tacs.py
+++ petprep/workflows/pet/tacs.py
@@ -17,17 +17,14 @@ def init_pet_tacs_wf(*, output_dir: str, metadata: dict, name: str = 'pet_tacs_w
 
     inputnode = pe.Node(
         niu.IdentityInterface(
-            fields=['source_file', 'pet_anat', 'segmentation', 'dseg_tsv']
+            fields=['source_file', 'pet_anat', 'segmentation', 'dseg_tsv', 'metadata']
         ),
         name='inputnode',
     )
     outputnode = pe.Node(niu.IdentityInterface(fields=['timeseries']), name='outputnode')
 
     tac = pe.Node(
-        ExtractTACs(
-            frame_times=timing_parameters.get('FrameTimesStart', []),
-            frame_durations=timing_parameters.get('FrameDuration', []),
-        ),
+        ExtractTACs(),
         name='tac',
         mem_gb=config.DEFAULT_MEMORY_MIN_GB,
     )
@@ -35,24 +32,32 @@ def init_pet_tacs_wf(*, output_dir: str, metadata: dict, name: str = 'pet_tacs_w
     ds_tac = pe.Node(
         DerivativesDataSink(
             base_directory=output_dir,
-            desc='timeactivity',
+            desc='preproc',
             suffix='timeseries',
+            **timing_parameters,
         ),
         name='ds_tac',
         run_without_submitting=True,
         mem_gb=config.DEFAULT_MEMORY_MIN_GB,
     )
 
-    workflow.connect([
-        (inputnode, tac, [
-            ('pet_anat', 'in_file'),
-            ('segmentation', 'segmentation'),
-            ('dseg_tsv', 'dseg_tsv'),
-        ]),
-        (inputnode, ds_tac, [('source_file', 'source_file')]),
-        (tac, ds_tac, [('out_file', 'in_file')]),
-        (tac, outputnode, [('out_file', 'timeseries')]),
-    ])
+    workflow.connect(
+            [
+                (
+                    inputnode,
+                    tac,
+                    [
+                        ('pet_anat', 'in_file'),
+                        ('segmentation', 'segmentation'),
+                        ('dseg_tsv', 'dseg_tsv'),
+                        ('metadata', 'metadata'),
+                    ],
+                ),
+                (inputnode, ds_tac, [('source_file', 'source_file')]),
+                (tac, ds_tac, [('out_file', 'in_file')]),
+                (tac, outputnode, [('out_file', 'timeseries')]),
+            ]
+        )
 
     return workflow
 
