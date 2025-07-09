ce2210d5787e8e982de6f3db3c63f62e64d8383e
mnoergaard <martin.noergaard@nru.dk>
2025-07-09 21:40:04 +0200

    FIX: add imports within function

--- petprep/workflows/pet/tacs.py
+++ petprep/workflows/pet/tacs.py
@@ -13,14 +13,17 @@ from ...interfaces import ExtractTACs
 def resample_pet_to_segmentation(pet_file: str, segmentation_file: str) -> str:
     """Resample the PET image to the segmentation space."""
 
+    from nilearn.image import resample_to_img
+    import os
+
     resampled_pet = resample_to_img(
         pet_file,
         segmentation_file,
         interpolation='continuous',
     )
-    out_file = Path('pet_resampled.nii.gz').absolute()
+    out_file = os.path.abspath('pet_resampled.nii.gz')
     resampled_pet.to_filename(out_file)
-    return str(out_file)
+    return out_file
 
 
 def init_pet_tacs_wf(*, name: str = 'pet_tacs_wf') -> pe.Workflow:
