3e5ba986af4b4a8077af669fcf66c6887dc98c82
mnoergaard <martin.noergaard@nru.dk>
2025-07-16 03:34:30 +0200

    FIX: Refactor test_pet_wf to use _prep_pet_series

--- petprep/workflows/pet/tests/test_base.py
+++ petprep/workflows/pet/tests/test_base.py
@@ -48,17 +48,8 @@ def test_pet_wf(
     output_dir = tmp_path / 'output'
     output_dir.mkdir()
 
-    img = nb.Nifti1Image(np.zeros((10, 10, 10, 10)), np.eye(4))
-
     if task == 'rest':
-        pet_series = [
-            str(bids_root / 'sub-01' / 'pet' / 'sub-01_task-rest_run-1_pet.nii.gz'),
-        ]
-
-
-    # The workflow will attempt to read file headers
-    for path in pet_series:
-        img.to_filename(path)
+        pet_series = _prep_pet_series(bids_root)
 
     # Toggle running recon-all
     freesurfer = bool(freesurfer)
